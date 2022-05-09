b2e960e85420bdb2c4f8a8e6a831f9c4e6c0f277
mgrygles <mgrygles@gmail.com>
2022-05-09 11:12:08 -0500

    updated several info (incl token)

--- src/main/java/PulsarClientPubTest.java
+++ src/main/java/PulsarClientPubTest.java
@@ -19,7 +19,7 @@ public class PulsarClientPubTest {
     //private static final String PULSAR_BROKER_URL = "pulsar://localhost:6650";
     private static final String PULSAR_BROKER_URL = "pulsar+ssl://pulsar-gcp-useast1.streaming.datastax.com:6651";
     private static final String TOPIC_NAME = "persistent://poctenant1/default/topic-sim";
-    private static final String CONNECTION_TOKEN = "75a16ab56c";
+    private static final String CONNECTION_TOKEN = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2NTIxMTA1NTksImlzcyI6ImRhdGFzdGF4Iiwic3ViIjoiY2xpZW50OzA3OTA3YTFmLWRiODItNDA0MC1hOTljLTE0YzgxNTExNmM5ZTtjRzlqZEdWdVlXNTBNUT09Ozc1YTE2YWI1NmMiLCJ0b2tlbmlkIjoiNzVhMTZhYjU2YyJ9.c8K-dY6h_8aox_O_aJYBUGXgZG--nMrKpNBnoMYMMF2qap49asCgSw71R8ZoOIZEmX4uhyZMpIv0XV0T6qPV_GFzmbb_OROt3NDb4i6dp3yvVuWEgsYRRy8GBiXIcDolhVVFDE9nmtJiMwZC0_zlbpxYMGHhePIX9STgxF5eEl0w2b01FrAy50k1EQfahaynLMip8insHHEnLMD-wBgPoEjpUgX_12grPtMi8CRQh5fg8YLgZFZ63OItGUNBT5YZoD7CxiXTUJifDUGLLTzYhRVXbQVg_Xoh6cWMtWJWX3DX8C1HUP2VL-J1NqHfOhE3nnrQaYlt4zbVbM3BB6bA9Q";
 
     public static void main(String[] args) throws IOException {
 
@@ -50,7 +50,8 @@ public class PulsarClientPubTest {
                .send();
              */
 
-            producer.send("emitting the message".getBytes(StandardCharsets.UTF_8));
+            //producer.send("emitting the message".getBytes(StandardCharsets.UTF_8));
+            producer.send("EMITTING THE MESSAGE");
             LOGGER.debug("Published the message");
         } catch (PulsarClientException e) {
             LOGGER.error(e.toString());
